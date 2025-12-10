e95ed869900b09f853cfd8524c5d792aa31337b9
Meiw0 <229660710+Meiw0@users.noreply.github.com>
2025-12-10 10:22:48 +0700

    Update K1.py
    Change some detail because I input my real name 🙂

--- K1.py
+++ K1.py
@@ -15,19 +15,19 @@ frame_data.pack()
 
 tk.Label(frame_data, text="Nama:", font=("Arial", 12)).grid(row=0, column=0, sticky="w", padx=10, pady=5)
 tk.Label(frame_data, text=":", font=("Arial", 12)).grid(row=0, column=1)
-tk.Label(frame_data, text="Muhammad Nur Faiz", font=("Arial", 12, "bold")).grid(row=0, column=2, sticky="w", padx=10)
+tk.Label(frame_data, text="Masukkan nama mu", font=("Arial", 12, "bold")).grid(row=0, column=2, sticky="w", padx=10)
 
 tk.Label(frame_data, text="NIM:", font=("Arial", 12)).grid(row=1, column=0, sticky="w", padx=10, pady=5)
 tk.Label(frame_data, text=":", font=("Arial", 12)).grid(row=1, column=1)
-tk.Label(frame_data, text="L200250202", font=("Arial", 12, "bold")).grid(row=1, column=2, sticky="w", padx=10)
+tk.Label(frame_data, text="Masukkan NIM mu", font=("Arial", 12, "bold")).grid(row=1, column=2, sticky="w", padx=10)
 
 tk.Label(frame_data, text="Buku Favorit:", font=("Arial", 12)).grid(row=2, column=0, sticky="w", padx=10, pady=5)
 tk.Label(frame_data, text=":", font=("Arial", 12)).grid(row=2, column=1)
-tk.Label(frame_data, text="Maling Kundang", font=("Arial", 12, "bold")).grid(row=2, column=2, sticky="w", padx=10)
+tk.Label(frame_data, text="Masukkan Buku Favorit", font=("Arial", 12, "bold")).grid(row=2, column=2, sticky="w", padx=10)
 
 tk.Label(frame_data, text="Motto Hidup:", font=("Arial", 12)).grid(row=3, column=0, sticky="w", padx=10, pady=5)
 tk.Label(frame_data, text=":", font=("Arial", 12)).grid(row=3, column=1)
-tk.Label(frame_data, text="Pantang nyerah sebelum error ditemukan", font=("Arial", 12, "bold")).grid(row=3, column=2, sticky="w", padx=10)
+tk.Label(frame_data, text="Masukkan Motto Hidup Mu", font=("Arial", 12, "bold")).grid(row=3, column=2, sticky="w", padx=10)
 
 btn_close = tk.Button(window, text="Close", command=close)
 btn_close.pack(pady=10)
